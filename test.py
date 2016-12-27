efedb618ecbac57fa6f840deafb582ec6897b011
ckckck <billy104@naver.com>
2016-12-27 22:58:26 +0900

    i think im genius

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print ("hello")
+print("which one do you need?")
+cal = int(input("1.add 2.subtract 3.multifly 4.devide : "))
+first_number = int(input("first number : "))
+second_number = int(input("second number : "))
+
+if cal == 1:
+    print(first_number + second_number)
+elif cal == 2:
+    print(first_number - second_number)
+elif cal == 3:
+    print(first_number * second_number)
+elif cal == 4:
+    print(first_number / second_number)
