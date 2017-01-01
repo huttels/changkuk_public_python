e824743340de58184b1f8d2b9ff91540b8c518d2
ckckck <billy104@naver.com>
2017-01-01 18:21:14 +0900

    shibal nan jotbab

--- ck_bulls and cows.py
+++ ck_bulls and cows.py
@@ -1,6 +1,6 @@
-
-
 import random
+
+
 # answer = []
 # b = [1,2,3]
 #
@@ -11,18 +11,19 @@ import random
 #     answer.append(random.randrange(0,10))
 #     answer = list(set(answer))
 
-def ohyeah() :
+def ohyeah():
     answer = []
-    b = [1, 2, 3]
 
-    for a in b:
-        answer.append(random.randrange(0, 10))
+    for a in range(1,4):
+        answer.append(random.randrange(1, 10))
     answer = list(set(answer))
     while len(answer) != 3:
-        answer.append(random.randrange(0, 10))
+        answer.append(random.randrange(1, 10))
         answer = list(set(answer))
     return answer
-c=ohyeah()
+
+
+c = ohyeah()
 '''
 ck = 0
 guess = []
@@ -32,7 +33,9 @@ while ck < 3 :
     m2 = int(m)
     guess.append(m2)
 '''
-def goguess() :
+
+
+def goguess():
     ck = 0
     guess = []
     while ck < 3:
@@ -41,23 +44,32 @@ def goguess() :
         m2 = int(m)
         guess.append(m2)
     return guess
-d = goguess()
+
 
 # 너가 넣은 숫자 3개 와 지렸당
-#리스트 - 위치 확인(index) 함수
-#같은숫자있으면 볼
-#같은숫자에같은 위치면 스트랔
-cj = 0
-guess = []
-while c != guess :
-    goguess()
-    if c[0] in guess :
-        print("볼입니다")
-    elif c[1] in guess :
-        print()
+# 리스트 - 위치 확인(index) 함수
+# 같은숫자있으면 볼
+# 같은숫자에같은 위치면 스트랔
 
 
 
 
+kk =0
+d = []
+while c != d:
+    kk= kk+1
+    pp = 0
+    ap = 0
+    d = goguess()
+    for i in range(0,3):
 
+        if c[i] == d[i]:
+            pp = pp + 1
+
+        elif c[i] in d:
+            ap = ap + 1
+
+    print("{}스트라이크{}볼 입니다".format(pp, ap))
+
+print("축하합니다!!! {}턴만에 맞추셨습니다~~".format(kk))
 
--- practice.py
+++ practice.py
@@ -1,6 +1,17 @@
-a = [1,2,3]
+c = [1,2,3]
+d= [3,4,5]
 
-if 4 in a:
-    print("yeah")
-else:
-    print("no")
+for i in c:
+    if i in d:
+        print("볼입니다")
+
+        while True:
+            d = goguess()
+            if c[0] == d[0]:
+                pp = pp + 1
+                print("{}스트라이크입니다.".format(pp))
+            elif c[0] in d:
+                ap = ap + 1
+                print("{}볼입니다".format(ap))
+            else:
+                print("{}스트라이크{}볼 입니다".format(pp, ap))
