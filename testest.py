7833ce8416be9251fe8ad8673c4ad797223196e6
ckckck <billy104@naver.com>
2017-01-09 20:57:37 +0900

    jotbab.. ^^

--- testest.py
+++ testest.py
@@ -1,10 +1,18 @@
-import random
+a = int(input("2진법으로 만들 숫자를 입렬하시옹 : "))
+b = []
+while a/2 >= 1 :
+    b.append(int(a)%int(2))    # 여기서 b.append(str(int(a)%int(2)))  왜 안됨요??
+    a = a/2
+b.append(1)                   # str(b) -> print("".join(b))왜 안됨요??
 
-answer = []
-number_array = list(range(1, 11))
-random.shuffle(number_array)
-print(number_array)
+c=[]
+for i in b:
+    c.append(str(i))
+c.reverse()
+print("".join(c))
 
-answer = number_array[0:3]
-
-print(answer)
+'''
+http://blog.eairship.kr/285
+초기화 ???
+set.name =name 이 왜 초기화??
+name값을 집어넣는거 아님???
